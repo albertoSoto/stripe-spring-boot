f1223ea344bc6f32029a48c29eb816c0a24bffee
Alberto Soto <>
2021-12-03 22:35:35 +0100

    injected properties - test

--- src/test/java/com/hamdibouallegue/stripe/StripeApplicationTests.java
+++ src/test/java/com/hamdibouallegue/stripe/StripeApplicationTests.java
@@ -1,13 +1,23 @@
 package com.hamdibouallegue.stripe;
 
+import com.hamdibouallegue.stripe.settings.StripeSettings;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
+@EnableConfigurationProperties(StripeSettings.class)
 class StripeApplicationTests {
+
+	@Autowired
+	StripeSettings stripeSettings;
 
 	@Test
 	void contextLoads() {
+		Assert.isTrue(StringUtils.equals(stripeSettings.getUsername(),"foo"),"Settings not loaded");
 	}
 
 }
